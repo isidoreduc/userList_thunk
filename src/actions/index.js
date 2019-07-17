c3d5e6734e4118b730006360ba32cc067f3b91ec
isidoreduc <isidoreducasse79@gmail.com>
2019-07-17 21:50:22 +0200

    solving API overcalling issues

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+import _ from 'lodash';
 
 //thunk just lets us get server data async by simply returning a function that creates an action object
 export const fetchPosts = () => async dispatch => {
@@ -9,4 +10,19 @@ export const fetchPosts = () => async dispatch => {
 export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+// using lodash functions to reduce number of API calls for users
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
+
+  // alternative to the chain syntax
+  // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  // userIds.forEach(id => dispatch(fetchUser(id)));
 };
--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchUser } from '../actions';
 
 class UserHeader extends React.Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId);
-  }
-
   render() {
-    const userino = this.props.myUsers.find(u => u.id === this.props.userId);
+    const userino = this.props.myUser;
     if (!userino) return null;
     return <div className='header'>{userino.name}</div>;
   }
 }
 
-const mapStateToProps = state => {
-  return { myUsers: state.users };
+const mapStateToProps = (state, ownProps) => {
+  return { myUser: state.users.find(u => u.id === ownProps.userId) };
 };
 
-export default connect(
-  mapStateToProps,
-  { fetchUser }
-)(UserHeader);
+export default connect(mapStateToProps)(UserHeader);
